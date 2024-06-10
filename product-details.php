7394adfc1ba89f0e91001548f385d231244ebaf8
mvinduwara <mvinduwara@gmail.com>
2024-06-10 11:08:14 +0530

    new update

--- product-details.php
+++ product-details.php
@@ -377,10 +377,10 @@ if (isset($_GET["id"])) {
 
                                         <div class="ht-product-action">
                                             <ul>
-                                                <li><a href="#" data-toggle="modal" data-target="#exampleModal"><i class="sli sli-magnifier"></i><span class="ht-product-action-tooltip">Quick View</span></a></li>
-                                                <li><a href="#"><i class="sli sli-heart"></i><span class="ht-product-action-tooltip">Add to Wishlist</span></a></li>
-                                                <li><a href="compare-page.php?id=<?php echo $product_type_data["product_id"]; ?>&model=<?php echo $product_type_data['product_model_has_brand_id']; ?>"><i class="sli sli-refresh"></i><span class="ht-product-action-tooltip">Add to Compare</span></a></li>
-                                                <li><a href="#"><i class="sli sli-bag"></i><span class="ht-product-action-tooltip">Add to Cart</span></a></li>
+                                                <!-- <li><a href="#" data-toggle="modal" data-target="#exampleModal"><i class="sli sli-magnifier"></i><span class="ht-product-action-tooltip">Quick View</span></a></li> -->
+                                                <!-- <li><a href="#"><i class="sli sli-heart"></i><span class="ht-product-action-tooltip">Add to Wishlist</span></a></li> -->
+                                                <!-- <li><a href="compare-page.php?id=<?php echo $product_type_data["product_id"]; ?>&model=<?php echo $product_type_data['product_model_has_brand_id']; ?>"><i class="sli sli-refresh"></i><span class="ht-product-action-tooltip">Add to Compare</span></a></li> -->
+                                                <!-- <li><a href="#"><i class="sli sli-bag"></i><span class="ht-product-action-tooltip">Add to Cart</span></a></li> -->
                                             </ul>
                                         </div>
                                     </div>
