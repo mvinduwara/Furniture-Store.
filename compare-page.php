927654577c467d91afb94fd07fafb58d7152b000
mvinduwara <mvinduwara@gmail.com>
2024-06-17 21:44:25 +0530

    update

--- compare-page.php
+++ compare-page.php
@@ -16,11 +16,7 @@ if (isset($_GET["id"]) && isset($_GET["model"])) {
         <title>Parlo</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
-
-        <!-- CSS
-	============================================ -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png"> 
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
